64e6e060d00ba16d7efab51c5081ec44ec82df9b
jinee <sksowk156@gmail.com>
2022-12-27 21:58:37 +0900

    retrofit_marketplace_coroutine

--- app/src/main/java/com/umc/second_week/data/remote/marketplace/interface/MarketplaceRetrofitInterface.kt
+++ app/src/main/java/com/umc/second_week/data/remote/marketplace/interface/MarketplaceRetrofitInterface.kt
@@ -1,10 +1,10 @@
 package com.umc.second_week.data.remote.marketplace.`interface`
 
 import com.umc.second_week.data.remote.marketplace.dto.MarketplaceDTO
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface MarketplaceRetrofitInterface {
     @GET("users/Kotlin/repos")
-    fun kotlinUsers() : Call<MarketplaceDTO>
+    suspend fun kotlinUsers() : Response<MarketplaceDTO>
 }
--- app/src/main/java/com/umc/second_week/ui/main/external/allboard/marketplace/MarketplaceFragment.kt
+++ app/src/main/java/com/umc/second_week/ui/main/external/allboard/marketplace/MarketplaceFragment.kt
@@ -3,17 +3,20 @@ package com.umc.second_week.ui.main.external.allboard.marketplace
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.second_week.R
-import com.umc.second_week.data.remote.marketplace.`interface`.MarketplaceRetrofitInterface
 import com.umc.second_week.data.remote.marketplace.dto.MarketplaceDTO
+import com.umc.second_week.data.remote.marketplace.service.MarketplaceService
 import com.umc.second_week.data.remote.marketplace.service.MarketplaceService.marketplaceservice
 import com.umc.second_week.databinding.FragmentMarketplaceBinding
 import com.umc.second_week.ui.BaseFragment
 import com.umc.second_week.ui.main.external.allboard.marketplace.adapter.MarketplaceAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MarketplaceFragment :
     BaseFragment<FragmentMarketplaceBinding>(R.layout.fragment_marketplace) {
@@ -28,31 +31,38 @@ class MarketplaceFragment :
             marketplaceRecyclerview.adapter = marketplaceAdapter
             marketplaceRecyclerview.layoutManager = LinearLayoutManager(context)
 
-//            // 2. 레트로핏 생성
-//            val retrofit = Retrofit.Builder()
-//                .baseUrl("https://api.github.com")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build()
+            // 코루틴
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = marketplaceservice.kotlinUsers()
 
-//            // 3. MarketplaceRetrofitInterface(interface)를 실제 사용가능한 코드로 변환
-//            val marketplaceRetrofitInterface = retrofit.create(MarketplaceRetrofitInterface::class.java)
-
-            // 4. 버튼 클릭 시 데이터를 가져와서 어댑터에 달아준다.
-            marketplaceservice.kotlinUsers().enqueue(object : Callback<MarketplaceDTO> {
-                override fun onResponse(
-                    call: Call<MarketplaceDTO>,
-                    response: Response<MarketplaceDTO>
-                ) {
-                    response.body()?.let { result ->
-                        marketplaceAdapter.userList = result
-                        marketplaceAdapter.notifyDataSetChanged()
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { result ->
+                            marketplaceAdapter.userList = result
+                            marketplaceAdapter.notifyDataSetChanged()
+                        }
+                    } else {
+                        Log.d("TAG", response.code().toString())
                     }
                 }
+            }
 
-                override fun onFailure(call: Call<MarketplaceDTO>, t: Throwable) {
-                    Log.e("에러에러", "${t.localizedMessage}")
-                }
-            })
+//          //   기본 Retrofit의 비동기처리햣
+//            marketplaceservice.kotlinUsers().enqueue(object : Callback<MarketplaceDTO> {
+//                override fun onResponse(
+//                    call: Call<MarketplaceDTO>,
+//                    response: Response<MarketplaceDTO>
+//                ) {
+//                    response.body()?.let { result ->
+//                        marketplaceAdapter.userList = result
+//                        marketplaceAdapter.notifyDataSetChanged()
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<MarketplaceDTO>, t: Throwable) {
+//                    Log.e("에러에러", "${t.localizedMessage}")
+//                }
+//            })
         }
     }
 }
